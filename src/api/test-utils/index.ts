d198a28868a8f58392aeed101d6171e95a063ec5
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-07-02 20:20:20 -0500

    Remove unused shared utils

--- src/api/test-utils/index.ts
+++
@@ -1,4 +0,0 @@
-export * from "./mocks";
-export * from "./mockSetup";
-export { createGetBelongClientMock } from "./getBelongClientMock";
-export { createWrapper } from "./react-query-wrapper";
--- src/shared/utils/index.ts
+++ src/shared/utils/index.ts
@@ -2,5 +2,6 @@ export * from './distance';
 export * from './storage';
 export * from './transformers';
 export * from './postgis';
+export * from './auth-helpers';
 
 // Might export edge later
