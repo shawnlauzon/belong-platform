0a629562f111e7cac3903991185969acf32a405b
Shawn Lauzon <shawn.lauzon@gmail.com>
2025-09-08 13:18:52 -0500

    fix: useUnreadCounts is missing

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export * from './config';
 export * from './features';
 export * from './shared/types/coordinates';
 export * from './shared/hooks/useSupabase';
+export * from './shared/hooks/useUnreadCounts';
